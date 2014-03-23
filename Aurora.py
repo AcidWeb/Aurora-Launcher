40569efa405622fa8ed7e316be5d9a9e74c2ddd7
Paweł Jastrzębski <pawelj@vulturis.eu>
2014-03-23 17:20:13 +0100

    Little cleanup

--- Aurora.py
+++ Aurora.py
@@ -4,50 +4,50 @@ import winreg
 import subprocess
 import win32file
 
-# Enviroment setup
+REG = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Control Panel\\International', 0, winreg.KEY_ALL_ACCESS)
 DATABASE_PASSWORD = ''
-if os.path.isfile(os.path.join(os.getcwd(), 'unins000.exe')):
+CWD = os.getcwd()
+if os.path.isfile(os.path.join(CWD, 'unins000.exe')):
     IS_PORTABLE = False
 else:
     IS_PORTABLE = True
-k = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Control Panel\\International', 0, winreg.KEY_ALL_ACCESS)
-kDecimalOrg = winreg.QueryValueEx(k, 'sDecimal')[0]
-kThousandOrg = winreg.QueryValueEx(k, 'sThousand')[0]
-winreg.SetValueEx(k, 'sDecimal', 0, 1, '.')
-winreg.SetValueEx(k, 'sThousand', 0, 1, ' ')
+
+# Enviroment setup
+regDecimalOrg = winreg.QueryValueEx(REG, 'sDecimal')[0]
+regThousandOrg = winreg.QueryValueEx(REG, 'sThousand')[0]
+winreg.SetValueEx(REG, 'sDecimal', 0, 1, '.')
+winreg.SetValueEx(REG, 'sThousand', 0, 1, ' ')
 if IS_PORTABLE:
-    p = subprocess.Popen('regsvr32 /s ' + os.path.join(os.getcwd(), 'MSSTDFMT.DLL'))
+    p = subprocess.Popen('regsvr32 /s ' + os.path.join(CWD, 'MSSTDFMT.DLL'))
     p.wait()
-if os.path.isfile(os.path.join(os.getcwd(), 'Stevefire.mdb.4')):
-    shutil.copyfile(os.path.join(os.getcwd(), 'Stevefire.mdb.4'), os.path.join(os.getcwd(), 'Stevefire.mdb.5'))
-if os.path.isfile(os.path.join(os.getcwd(), 'Stevefire.mdb.3')):
-    shutil.copyfile(os.path.join(os.getcwd(), 'Stevefire.mdb.3'), os.path.join(os.getcwd(), 'Stevefire.mdb.4'))
-if os.path.isfile(os.path.join(os.getcwd(), 'Stevefire.mdb.2')):
-    shutil.copyfile(os.path.join(os.getcwd(), 'Stevefire.mdb.2'), os.path.join(os.getcwd(), 'Stevefire.mdb.3'))
-shutil.copyfile(os.path.join(os.getcwd(), 'Stevefire.mdb'), os.path.join(os.getcwd(), 'Stevefire.mdb.2'))
-os.remove(os.path.join(os.getcwd(), 'Stevefire.mdb'))
-p = subprocess.Popen(os.path.join(os.getcwd(), 'Tools', 'JETCOMP.exe')
-                     + ' -src:"' + os.path.join(os.getcwd(), 'Stevefire.mdb.2')
-                     + '" -dest:"' + os.path.join(os.getcwd(), 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD)
+if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.4')):
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.4'), os.path.join(CWD, 'Stevefire.mdb.5'))
+if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.3')):
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.3'), os.path.join(CWD, 'Stevefire.mdb.4'))
+if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.2')):
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.2'), os.path.join(CWD, 'Stevefire.mdb.3'))
+shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb'), os.path.join(CWD, 'Stevefire.mdb.2'))
+os.remove(os.path.join(CWD, 'Stevefire.mdb'))
+p = subprocess.Popen(os.path.join(CWD, 'Tools', 'JETCOMP.exe') + ' -src:"' + os.path.join(CWD, 'Stevefire.mdb.2')
+                     + '" -dest:"' + os.path.join(CWD, 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD)
 p.wait()
 try:
-    win32file.CreateSymbolicLink(os.path.splitdrive(os.getcwd())[0] + os.path.sep + 'Logs',
-                                 os.path.join(os.getcwd(), 'Logs'), 1)
+    win32file.CreateSymbolicLink(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs', os.path.join(CWD, 'Logs'), 1)
 except:
     pass
 
 # Starting Aurora
-p = subprocess.Popen(os.path.join(os.getcwd(), 'Aurora.exe'))
+p = subprocess.Popen(os.path.join(CWD, 'Aurora.exe'))
 p.wait()
 
 # Cleanup
 if IS_PORTABLE:
-    p = subprocess.Popen('regsvr32 /s /u ' + os.path.join(os.getcwd(), 'MSSTDFMT.DLL'))
+    p = subprocess.Popen('regsvr32 /s /u ' + os.path.join(CWD, 'MSSTDFMT.DLL'))
     p.wait()
 try:
-    os.remove(os.path.splitdrive(os.getcwd())[0] + os.path.sep + 'Logs')
+    os.remove(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs')
 except:
     pass
-winreg.SetValueEx(k, 'sDecimal', 0, 1, kDecimalOrg)
-winreg.SetValueEx(k, 'sThousand', 0, 1, kThousandOrg)
-k.Close()
+winreg.SetValueEx(REG, 'sDecimal', 0, 1, regDecimalOrg)
+winreg.SetValueEx(REG, 'sThousand', 0, 1, regThousandOrg)
+REG.Close()
