bf9c8a21c9b556cc78c318fb71deb62b939c0cc6
Paweł Jastrzębski <pawelj@iosphe.re>
2016-01-02 20:27:05 +0100

    Improved error handling

--- Aurora.py
+++ Aurora.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import shutil
 import winreg
 import subprocess
 import win32file
 
-VERSION = '1.4'
+VERSION = '1.4.1'
 REG = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Control Panel\\International', 0, winreg.KEY_ALL_ACCESS)
 DATABASE_PASSWORD = ''
 CWD = os.getcwd()
@@ -12,6 +13,20 @@ if os.path.isfile(os.path.join(CWD, 'unins000.exe')):
     IS_PORTABLE = False
 else:
     IS_PORTABLE = True
+
+# Database backup
+try:
+    shutil.move(os.path.join(CWD, 'Stevefire.mdb'), os.path.join(CWD, 'Stevefire.mdb.2'))
+except PermissionError:
+    sys.exit()
+if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.4')):
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.4'), os.path.join(CWD, 'Stevefire.mdb.5'))
+if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.3')):
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.3'), os.path.join(CWD, 'Stevefire.mdb.4'))
+shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.2'), os.path.join(CWD, 'Stevefire.mdb.3'))
+p = subprocess.Popen(os.path.join(CWD, 'Tools', 'JETCOMP.exe') + ' -src:"' + os.path.join(CWD, 'Stevefire.mdb.2') +
+                     '" -dest:"' + os.path.join(CWD, 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD)
+p.wait()
 
 # Enviroment setup
 regDecimalOrg = winreg.QueryValueEx(REG, 'sDecimal')[0]
@@ -21,17 +36,6 @@ winreg.SetValueEx(REG, 'sThousand', 0, 1, ' ')
 if IS_PORTABLE:
     p = subprocess.Popen('regsvr32 /s "' + os.path.join(CWD, 'MSSTDFMT.DLL') + '"')
     p.wait()
-if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.4')):
-    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.4'), os.path.join(CWD, 'Stevefire.mdb.5'))
-if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.3')):
-    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.3'), os.path.join(CWD, 'Stevefire.mdb.4'))
-if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.2')):
-    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.2'), os.path.join(CWD, 'Stevefire.mdb.3'))
-shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb'), os.path.join(CWD, 'Stevefire.mdb.2'))
-os.remove(os.path.join(CWD, 'Stevefire.mdb'))
-p = subprocess.Popen(os.path.join(CWD, 'Tools', 'JETCOMP.exe') + ' -src:"' + os.path.join(CWD, 'Stevefire.mdb.2') +
-                     '" -dest:"' + os.path.join(CWD, 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD)
-p.wait()
 try:
     win32file.CreateSymbolicLink(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs', os.path.join(CWD, 'Logs'), 1)
 except:
