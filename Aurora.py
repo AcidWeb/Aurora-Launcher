583f4a17f4679bb607d5329550c77d2eb9a6ce74
Paweł Jastrzębski <pawelj@iosphe.re>
2016-01-03 09:31:40 +0100

    Added splash screen

--- Aurora.py
+++ Aurora.py
@@ -4,8 +4,9 @@ import shutil
 import winreg
 import subprocess
 import win32file
+import tkinter as tk
 
-VERSION = '1.4.1'
+VERSION = '1.5'
 REG = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Control Panel\\International', 0, winreg.KEY_ALL_ACCESS)
 DATABASE_PASSWORD = ''
 CWD = os.getcwd()
@@ -14,45 +15,61 @@ if os.path.isfile(os.path.join(CWD, 'unins000.exe')):
 else:
     IS_PORTABLE = True
 
-# Database backup
-try:
-    shutil.move(os.path.join(CWD, 'Stevefire.mdb'), os.path.join(CWD, 'Stevefire.mdb.2'))
-except PermissionError:
-    sys.exit()
-if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.4')):
-    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.4'), os.path.join(CWD, 'Stevefire.mdb.5'))
-if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.3')):
-    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.3'), os.path.join(CWD, 'Stevefire.mdb.4'))
-shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.2'), os.path.join(CWD, 'Stevefire.mdb.3'))
-p = subprocess.Popen(os.path.join(CWD, 'Tools', 'JETCOMP.exe') + ' -src:"' + os.path.join(CWD, 'Stevefire.mdb.2') +
-                     '" -dest:"' + os.path.join(CWD, 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD)
-p.wait()
 
-# Enviroment setup
-regDecimalOrg = winreg.QueryValueEx(REG, 'sDecimal')[0]
-regThousandOrg = winreg.QueryValueEx(REG, 'sThousand')[0]
-winreg.SetValueEx(REG, 'sDecimal', 0, 1, '.')
-winreg.SetValueEx(REG, 'sThousand', 0, 1, ' ')
-if IS_PORTABLE:
-    p = subprocess.Popen('regsvr32 /s "' + os.path.join(CWD, 'MSSTDFMT.DLL') + '"')
-    p.wait()
-try:
-    win32file.CreateSymbolicLink(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs', os.path.join(CWD, 'Logs'), 1)
-except:
-    pass
+def aurora(r):
+    # Database backup
+    try:
+        shutil.move(os.path.join(CWD, 'Stevefire.mdb'), os.path.join(CWD, 'Stevefire.mdb.2'))
+    except PermissionError:
+        sys.exit()
+    if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.4')):
+        shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.4'), os.path.join(CWD, 'Stevefire.mdb.5'))
+    if os.path.isfile(os.path.join(CWD, 'Stevefire.mdb.3')):
+        shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.3'), os.path.join(CWD, 'Stevefire.mdb.4'))
+    shutil.copyfile(os.path.join(CWD, 'Stevefire.mdb.2'), os.path.join(CWD, 'Stevefire.mdb.3'))
+    subprocess.Popen(os.path.join(CWD, 'Tools', 'JETCOMP.exe') + ' -src:"' + os.path.join(CWD, 'Stevefire.mdb.2') +
+                     '" -dest:"' + os.path.join(CWD, 'Stevefire.mdb') + '" -w' + DATABASE_PASSWORD).wait()
 
-# Starting Aurora
-p = subprocess.Popen(os.path.join(CWD, 'Aurora.exe'))
-p.wait()
+    # Enviroment setup
+    regdecimalorg = winreg.QueryValueEx(REG, 'sDecimal')[0]
+    regthousandorg = winreg.QueryValueEx(REG, 'sThousand')[0]
+    winreg.SetValueEx(REG, 'sDecimal', 0, 1, '.')
+    winreg.SetValueEx(REG, 'sThousand', 0, 1, ' ')
+    if IS_PORTABLE:
+        subprocess.Popen('regsvr32 /s "' + os.path.join(CWD, 'MSSTDFMT.DLL') + '"').wait()
+    try:
+        win32file.CreateSymbolicLink(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs', os.path.join(CWD, 'Logs'), 1)
+    except:
+        pass
 
-# Cleanup
-if IS_PORTABLE:
-    p = subprocess.Popen('regsvr32 /s /u "' + os.path.join(CWD, 'MSSTDFMT.DLL') + '"')
-    p.wait()
-try:
-    os.remove(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs')
-except:
-    pass
-winreg.SetValueEx(REG, 'sDecimal', 0, 1, regDecimalOrg)
-winreg.SetValueEx(REG, 'sThousand', 0, 1, regThousandOrg)
-REG.Close()
+    # Starting Aurora
+    r.destroy()
+    subprocess.Popen(os.path.join(CWD, 'Aurora.exe')).wait()
+
+    # Cleanup
+    if IS_PORTABLE:
+        subprocess.Popen('regsvr32 /s /u "' + os.path.join(CWD, 'MSSTDFMT.DLL') + '"').wait()
+    try:
+        os.remove(os.path.splitdrive(CWD)[0] + os.path.sep + 'Logs')
+    except:
+        pass
+    winreg.SetValueEx(REG, 'sDecimal', 0, 1, regdecimalorg)
+    winreg.SetValueEx(REG, 'sThousand', 0, 1, regthousandorg)
+    REG.Close()
+
+
+if __name__ == "__main__":
+    # Show splash
+    root = tk.Tk()
+    root.overrideredirect(True)
+    w = 465
+    h = 215
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    image = tk.PhotoImage(file=os.path.join(CWD, 'Splash.png'))
+    root.geometry('%dx%d+%d+%d' % (w, h, (ws/2) - (w/2), (hs/2) - (h/2)))
+    canvas = tk.Canvas(root, width=w, height=h, bg='black')
+    canvas.create_image(w/2, h/2, image=image)
+    canvas.pack()
+    root.after(2000, aurora, root)
+    root.mainloop()
--- Setup.bat
+++ Setup.bat
@@ -1,2 +1,2 @@
-pyinstaller.exe -y -F -i Assets\Aurora.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
+pyinstaller -y -F -i Assets\Aurora.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
 copy Tools\Aurora_Wrapper.exe.manifest dist\
