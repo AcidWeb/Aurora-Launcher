599c1add2c6aac92fc8a3c3d6b84cc7ccf3de134
Paweł Jastrzębski <pawelj@iosphe.re>
2016-01-06 12:13:22 +0100

    Installer tweaks

--- Setup.bat
+++ Setup.bat
@@ -1,2 +1,1 @@
-pyinstaller -y -F -i Assets\Aurora.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
-copy Tools\Aurora_Wrapper.exe.manifest dist\
+pyinstaller -y -D -i Assets\Aurora.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
