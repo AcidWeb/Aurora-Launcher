6869121da7eabae9d2c7fe8f016e48839046c6d9
Paweł Jastrzębski <pawelj@iosphe.re>
2016-01-12 20:55:50 +0100

    New wrapper icon

--- Setup.bat
+++ Setup.bat
@@ -1,2 +1,2 @@
-pyinstaller -y -F -i Assets\Aurora.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
+pyinstaller -y -F -i Assets\AuroraWrapper.ico -n Aurora_Wrapper -w --noupx --uac-admin Aurora.py
 copy Tools\Aurora_Wrapper.exe.manifest dist\
